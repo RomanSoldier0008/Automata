aa25d8f087e7c89efa8eac2d2df9bb4088e9f6ec
Marius <49422572+RomanSoldier0008@users.noreply.github.com>
2023-05-26 13:14:44 -0300

    Resuelvo: posibilidad de ingresar + de una ipv4

---
+++ Trabajo 3/Ejercicio 3/main.py
@@ -0,0 +1,32 @@
+from constant import *
+from ipaddress import IPv4Address, AddressValueError
+
+class Excersice_c:
+    def __init__(self, adress_value=""):
+        self.__adress = adress_value
+
+    def add_ipv4(self):
+        self.__adress = str(input(ADD_IPV4_INPUT))
+
+    def is_valid_ipv4_address(self):
+        count = self.__adress.count(".")
+        try:
+            if count == 3:
+                IPv4Address(self.__adress)
+                return True
+            return False
+        except AddressValueError:
+            return False
+
+    def get_out_put(self):
+        if self.is_valid_ipv4_address():
+            self.print(IPV4_VALID_OUTPUT.format(self.__adress))
+        else:
+            self.print(IPV4_NOT_VALID_OUTPUT.format(self.__adress))
+
+    def print(self, value):
+        print(value)
+
+excersice_c = Excersice_c()
+excersice_c.add_ipv4()
+excersice_c.get_out_put()
